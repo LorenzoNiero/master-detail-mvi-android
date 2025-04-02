b6a3c43b2b8d8a73340967b099ab9c3fcd0c0dc3
Lorenzo Niero <niero.lorenzo@gmail.com>
2025-04-02 12:43:57 +0200

    Added `navigateToDetail` function to `ListViewModel` to handle the navigation to the detail screen.

--- feature/list/src/main/java/com/challenge/master_detail/list/presentation/ListScreen.kt
+++ feature/list/src/main/java/com/challenge/master_detail/list/presentation/ListScreen.kt
@@ -1,24 +1,25 @@
 package com.challenge.master_detail.list.presentation
 
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
-fun ListScreen(
-    viewModel: ListViewModel = hiltViewModel()
-) {
-
-    ListContent()
+fun ListScreen(viewModel: ListViewModel = hiltViewModel()) {
+    ListContent(
+        viewModel::navigateToDetail,
+    )
 }
 
 @Composable
-fun ListContent() {
-
+fun ListContent(onClick: () -> Unit = {}) {
     Text(text = "List")
+    Button(onClick = onClick) {
+        Text(text = "Detail")
+    }
 }
-
 
 @Preview(showBackground = true)
 @Composable
--- feature/list/src/main/java/com/challenge/master_detail/list/presentation/ListViewModel.kt
+++ feature/list/src/main/java/com/challenge/master_detail/list/presentation/ListViewModel.kt
@@ -1,12 +1,18 @@
 package com.challenge.master_detail.list.presentation
 
 import androidx.lifecycle.ViewModel
+import com.challenge.master_detail.navigator.Navigator
+import com.challenge.master_detail.navigator.destination.DetailNavigationDestination
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class ListViewModel @Inject constructor(
-
-) : ViewModel(){
-
-}
+class ListViewModel
+    @Inject
+    constructor(
+        private val navigator: Navigator,
+    ) : ViewModel(), Navigator by navigator {
+        fun navigateToDetail() {
+            navigator.navigate(DetailNavigationDestination)
+        }
+    }
