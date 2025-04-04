756a5262de8718fc5e6fb4eb7957f5f8b929f220
Lorenzo Niero <niero.lorenzo@gmail.com>
2025-04-04 10:47:07 +0200

    added unit tests and mock data for GetMediasUseCaseTest.kt

---
+++ core/domain/src/test/java/com/challenge/master_detail/domain/domain/usecase/GetMediasUseCaseTest.kt
@@ -0,0 +1,38 @@
+package com.challenge.master_detail.domain.domain.usecase
+
+import com.challenge.master_detail.domain.mock.DomainMock
+import com.challenge.master_detail.domain.repository.MediaRepository
+import com.challenge.master_detail.domain.usecase.GetMediasUseCase
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+
+class GetMediasUseCaseTest {
+    @MockK
+    private lateinit var repository: MediaRepository
+
+    private lateinit var useCase: GetMediasUseCase
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this, relaxUnitFun = true)
+        useCase = GetMediasUseCase(repository)
+    }
+
+    @Test
+    fun `use case should call repository without error`() = runTest {
+        // Given
+        coEvery { repository.fetchMediaList() } returns DomainMock.listMedias
+
+        // When
+        val response = useCase()
+
+        // Then
+        coVerify { repository.fetchMediaList() }
+    }
+
+}
--- core/domain/src/test/java/com/challenge/master_detail/domain/mock/DomainMock.kt
+++ core/domain/src/test/java/com/challenge/master_detail/domain/mock/DomainMock.kt
@@ -1,4 +1,19 @@
 package com.challenge.master_detail.domain.mock
 
+import com.challenge.master_detail.common.model.MediaModel
+import com.challenge.master_detail.common.model.MediaModelType
+import java.time.LocalDateTime
+
 object DomainMock {
+
+    val listMedias : List<MediaModel> = listOf(
+        MediaModel(
+            id = 1,
+            url = "https://website.xyz/namefile.pdf",
+            urlBig = "",
+            type = MediaModelType.PDF,
+            date = LocalDateTime.now(),
+            title = "Title",
+        )
+    )
 }
